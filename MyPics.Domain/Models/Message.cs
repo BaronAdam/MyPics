ea0dfc2a974d7b16005df8f835844e3ec76cc28b
Adam <adamo.b1997@gmail.com>
2021-02-13 18:15:20 +0100

    Update Message.cs

--- MyPics.Domain/Models/Message.cs
+++ MyPics.Domain/Models/Message.cs
@@ -8,7 +8,6 @@ namespace MyPics.Domain.Models
         public int UserId { get; set; }
         public int RecipientId { get; set; }
         public User User { get; set; }
-        public User Recipient { get; set; }
         [Encrypted] public string Content { get; set; }
         public bool IsPhoto { get; set; }
         [Encrypted] public string Url { get; set; }
