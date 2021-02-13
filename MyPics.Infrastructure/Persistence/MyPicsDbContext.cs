dd8dec2c4280cfad7d6370752a4f78da238993b4
Adam <adamo.b1997@gmail.com>
2021-02-13 14:47:58 +0100

    Add dbContext + encryptionKey

---
+++ MyPics.Infrastructure/Persistence/MyPicsDbContext.cs
@@ -0,0 +1,55 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.DataEncryption;
+using Microsoft.EntityFrameworkCore.DataEncryption.Providers;
+using Microsoft.Extensions.Configuration;
+using MyPics.Domain.Models;
+
+namespace MyPics.Infrastructure.Persistence
+{
+    public class MyPicsDbContext : DbContext
+    {
+        private readonly IEncryptionProvider _provider;
+
+        public MyPicsDbContext(DbContextOptions<MyPicsDbContext> options, IConfiguration configuration) : base(options)
+        {
+            var encryptionKey = Convert.FromBase64String(configuration.GetSection("AppSettings:EncryptionKey").Value);
+            _provider = new AesProvider(encryptionKey);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostLike> PostLikes { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<CommentLike> CommentLikes { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseEncryption(_provider);
+
+            modelBuilder.Entity<Follow>()
+                .HasKey(x => new {x.FollowingId, x.UserId});
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Following)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(x => x.Following)
+                .WithMany(x => x.Followers)
+                .HasForeignKey(x => x.FollowingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<PostLike>()
+                .HasKey(x => new {x.PostId, x.UserId});
+
+            modelBuilder.Entity<CommentLike>()
+                .HasKey(x => new {x.CommentId, x.UserId});
+        }
+    }
+}
