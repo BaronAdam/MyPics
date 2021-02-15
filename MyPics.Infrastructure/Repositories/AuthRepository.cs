2d9da7d810f965ddd419ad72c7ab260a8a9965d3
Adam <adamo.b1997@gmail.com>
2021-02-15 08:39:58 +0100

    Add AuthRepository

---
+++ MyPics.Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyPics.Domain.Models;
+using MyPics.Infrastructure.Interfaces;
+using MyPics.Infrastructure.Persistence;
+
+namespace MyPics.Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly MyPicsDbContext _context;
+
+        public AuthRepository(MyPicsDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<User> Register(User user, string password)
+        {
+            CreatePasswordHash(password, out var passwordHash, out var passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null) return null;
+
+            return VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt) ? user : null;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.Username == username);
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            return await _context.Users.AnyAsync(x => x.Email == email);
+        }
+        
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        private static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return !computedHash.Where((t, i) => t != passwordHash[i]).Any();
+        }
+    }
+}
