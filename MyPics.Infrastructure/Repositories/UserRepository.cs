baa7fc9555533ae37c720aa209c3802e68a9c99a
Adam <adamo.b1997@gmail.com>
2021-02-15 20:49:36 +0100

    Add UserRepository

---
+++ MyPics.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyPics.Domain.Models;
+using MyPics.Infrastructure.Interfaces;
+using MyPics.Infrastructure.Persistence;
+
+namespace MyPics.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MyPicsDbContext _context;
+
+        public UserRepository(MyPicsDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<User> GetUserById(int id)
+        {
+            try
+            {
+                return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public async Task<User> GetUserByUsername(string username)
+        {
+            try
+            {
+                return await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
