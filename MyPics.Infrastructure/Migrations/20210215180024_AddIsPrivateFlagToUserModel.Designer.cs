b61e8da5dbd2ed49d23f3138ac1eeb6c03f10cdc
Adam <adamo.b1997@gmail.com>
2021-02-15 19:01:00 +0100

    Add migration with IsPrivate flag in User model

--- MyPics.Infrastructure/Migrations/20210215180024_AddIsPrivateFlagToUserModel.Designer.cs
+++ MyPics.Infrastructure/Migrations/20210215180024_AddIsPrivateFlagToUserModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPics.Infrastructure.Persistence;
 
 namespace MyPics.Infrastructure.Migrations
 {
     [DbContext(typeof(MyPicsDbContext))]
-    partial class MyPicsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210215180024_AddIsPrivateFlagToUserModel")]
+    partial class AddIsPrivateFlagToUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyPics.Infrastructure/Migrations/20210215180024_AddIsPrivateFlagToUserModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPics.Infrastructure.Migrations
+{
+    public partial class AddIsPrivateFlagToUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivate",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivate",
+                table: "Users");
+        }
+    }
+}
