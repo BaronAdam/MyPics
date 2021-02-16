2bbeb89c37fc1b8ab46e401e907e7bab74af6cb4
Adam <adamo.b1997@gmail.com>
2021-02-16 17:18:10 +0100

    Create DependencyInjectionConfiguration class

---
+++ MyPics.Api/Configuration/DependencyInjectionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyPics.Infrastructure.Interfaces;
+using MyPics.Infrastructure.Repositories;
+
+namespace MyPics.Api.Configuration
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void ConfigureDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
