671e12cdd99f1bae31d78116c0de5b62fe4014e4
Adam <adamo.b1997@gmail.com>
2021-02-19 15:54:09 +0100

    Add DTO tests

---
+++ MyPics.Domain.Tests/DTOs/UserForRegisterDtoTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using MyPics.Domain.DTOs;
+using NUnit.Framework;
+
+namespace MyPics.Domain.Tests.DTOs
+{
+    [TestFixture]
+    public class UserForRegisterDtoTests
+    {
+        [Test]
+        public void TestAll_ExpectedResult()
+        {
+            var entity = new UserForRegisterDto
+            {
+                Username = "testUsername",
+                Password = "testPassword",
+                Email = "email@test.com",
+                DisplayName = "Test User"
+            };
+
+            entity.Should().NotBeNull();
+            entity.Username.Should().Be("testUsername");
+            entity.Password.Should().Be("testPassword");
+            entity.Email.Should().Be("email@test.com");
+            entity.DisplayName.Should().Be("Test User");
+        }
+    }
+}
