fe7a5250ed83dbbb5bfb16a1c375083c320b84d7
Adam <adamo.b1997@gmail.com>
2021-02-19 15:53:56 +0100

    Update UserForLoginDto.cs

--- MyPics.Domain/DTOs/UserForLoginDto.cs
+++ MyPics.Domain/DTOs/UserForLoginDto.cs
@@ -1,8 +1,12 @@
-﻿namespace MyPics.Domain.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPics.Domain.DTOs
 {
     public class UserForLoginDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
