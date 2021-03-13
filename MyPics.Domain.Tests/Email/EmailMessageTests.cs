304638ec56e2ce24859f4be00aea5e3759eae99c
Adam <adamo.b1997@gmail.com>
2021-03-13 19:17:19 +0100

    Add EmailConfigurationTests and EmailMessageTests + move some tests

---
+++ MyPics.Domain.Tests/Email/EmailMessageTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using MimeKit;
+using MyPics.Domain.Email;
+using NUnit.Framework;
+
+namespace MyPics.Domain.Tests.Email
+{
+    [TestFixture]
+    public class EmailMessageTests
+    {
+        [Test]
+        public void TestAll_ExpectedResult()
+        {
+            var message = new EmailMessage
+            {
+                Sender = new MailboxAddress("testSender", "test@email.com"),
+                Receiver = new MailboxAddress("testReceiver", "test1@email.com"),
+                Subject = "testSubject",
+                Content = "testContent"
+            };
+
+            message.Should().NotBeNull();
+            message.Sender.Name.Should().Be("testSender");
+            message.Sender.Address.Should().Be("test@email.com");
+            message.Receiver.Name.Should().Be("testReceiver");
+            message.Receiver.Address.Should().Be("test1@email.com");
+            message.Subject.Should().Be("testSubject");
+            message.Content.Should().Be("testContent");
+        }
+    }
+}
