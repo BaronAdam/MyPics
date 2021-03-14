65496e635e9910ff3d16e19fe4e6379a78ee4b34
Adam <adamo.b1997@gmail.com>
2021-03-14 21:54:13 +0100

    Create UserParametersTests.cs

---
+++ MyPics.Infrastructure.Tests/Helpers/PaginationParameters/UserParametersTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using MyPics.Infrastructure.Helpers.PaginationParameters;
+using NUnit.Framework;
+
+namespace MyPics.Infrastructure.Tests.Helpers.PaginationParameters
+{
+    [TestFixture]
+    public class UserParametersTests
+    {
+        [TestCase(10)]
+        [TestCase(30)]
+        [TestCase(60)]
+        public void TestAll_ExpectedResult(int pageSize)
+        {
+            var result = new UserParameters
+            {
+                PageNumber = 1,
+                PageSize = pageSize
+            };
+
+            result.PageNumber.Should().Be(1);
+            result.PageSize.Should().BePositive();
+            result.PageNumber.Should().BeLessOrEqualTo(50);
+        }
+    }
+}
