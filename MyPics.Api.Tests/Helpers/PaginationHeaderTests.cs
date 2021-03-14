29df2b0fa6d2eacf62a69ca3ff3b7af5364882eb
Adam <adamo.b1997@gmail.com>
2021-03-14 21:53:56 +0100

    Add tests + update UserParameters

---
+++ MyPics.Api.Tests/Helpers/PaginationHeaderTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using MyPics.Api.Helpers;
+using NUnit.Framework;
+
+namespace MyPics.Api.Tests.Helpers
+{
+    [TestFixture]
+    public class PaginationHeaderTests
+    {
+        [Test]
+        public void TestAll_FromConstructor_ExpectedResult()
+        {
+            var result = new PaginationHeader(1, 2, 3, 4);
+
+            result.Should().NotBeNull();
+            result.CurrentPage.Should().Be(1);
+            result.ItemsPerPage.Should().Be(2);
+            result.TotalItems.Should().Be(3);
+            result.TotalPages.Should().Be(4);
+        }
+
+        [Test]
+        public void TestAll_FromSetters_ExpectedResult()
+        {
+            var result = new PaginationHeader(0, 0, 0, 0)
+            {
+                CurrentPage = 1, ItemsPerPage = 2, TotalItems = 3, TotalPages = 4
+            };
+
+            result.Should().NotBeNull();
+            result.CurrentPage.Should().Be(1);
+            result.ItemsPerPage.Should().Be(2);
+            result.TotalItems.Should().Be(3);
+            result.TotalPages.Should().Be(4);
+        }
+    }
+}
---
+++ MyPics.Infrastructure.Tests/Helpers/PagedListTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace MyPics.Infrastructure.Tests.Helpers
+{
+    [TestFixture]
+    public class PagedListTests
+    {
+        
+    }
+}
