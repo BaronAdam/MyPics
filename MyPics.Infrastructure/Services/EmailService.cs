735b9fc57f5792e7bee239af37a3b224a9392d37
Adam <adamo.b1997@gmail.com>
2021-03-16 10:01:40 +0100

    Update EmailService.cs

--- MyPics.Infrastructure/Services/EmailService.cs
+++ MyPics.Infrastructure/Services/EmailService.cs
@@ -18,29 +18,8 @@ namespace MyPics.Infrastructure.Services
             _configuration = configuration;
             
             _client = client;
-            try
-            {
-                _client.Connect(_configuration.SmtpServer, _configuration.Port, true);
-                _client.Authenticate(_configuration.UserName, _configuration.Password);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
         }
 
-        ~EmailService()
-        {
-            try
-            {
-                _client.Disconnect(true);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-        
         public async Task<bool> SendEmail(EmailMessage message)
         {
             message.Sender = new MailboxAddress(_configuration.SenderName, _configuration.Sender);
@@ -51,7 +30,10 @@ namespace MyPics.Infrastructure.Services
             
             try
             {
+                await _client.ConnectAsync(_configuration.SmtpServer, _configuration.Port, true);
+                await _client.AuthenticateAsync(_configuration.UserName, _configuration.Password);
                 await _client.SendAsync(mimeMessage);
+                await _client.DisconnectAsync(true);
             }
             catch (Exception e)
             {
