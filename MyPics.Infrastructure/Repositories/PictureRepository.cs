be9696a13edd7f46eccd766235e154d90b7c4460
Adam <adamo.b1997@gmail.com>
2021-04-07 09:43:24 +0200

    Add PictureRepository + PostRepository

---
+++ MyPics.Infrastructure/Repositories/PictureRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyPics.Domain.Models;
+using MyPics.Infrastructure.Interfaces;
+using MyPics.Infrastructure.Persistence;
+
+namespace MyPics.Infrastructure.Repositories
+{
+    public class PictureRepository : IPictureRepository
+    {
+        private readonly MyPicsDbContext _context;
+
+        public PictureRepository(MyPicsDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<bool> AddPicturesForPost(List<Picture> pictures)
+        {
+            try
+            {
+                await _context.Pictures.AddRangeAsync(pictures);
+                return await _context.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
---
+++ MyPics.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyPics.Domain.DTOs;
+using MyPics.Domain.Models;
+using MyPics.Infrastructure.Interfaces;
+using MyPics.Infrastructure.Persistence;
+
+namespace MyPics.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly MyPicsDbContext _context;
+
+        public PostRepository(MyPicsDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Post> AddPost(Post post)
+        {
+            Post result;
+            
+            try
+            {
+                result = _context.Posts.AddAsync(post).Result.Entity;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+
+            return result;
+        }
+
+        public async Task<bool> EditPost(PostForUpdateDto post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> DeletePost(int postId)
+        {
+            try
+            {
+                var post = _context.Posts.FirstOrDefault(x => x.Id == postId);
+                var pictures = _context.Pictures.Where(x => x.PostId == postId);
+
+                _context.Posts.Remove(post);
+                _context.Pictures.RemoveRange(pictures);
+
+                return await _context.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        private async Task<Post> GetPostById(int postId)
+        {
+            try
+            {
+                return await _context.Posts.FirstOrDefaultAsync(x => x.Id == postId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
