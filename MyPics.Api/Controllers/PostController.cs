0db0c10396d3780734d8dcfc8971bbb8ef24698d
Adam <adamo.b1997@gmail.com>
2021-04-07 09:43:40 +0200

    Add PostController

---
+++ MyPics.Api/Controllers/PostController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyPics.Domain.DTOs;
+using MyPics.Domain.Models;
+using MyPics.Infrastructure.Interfaces;
+
+namespace MyPics.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IPictureRepository _pictureRepository;
+        private readonly ICloudinaryService _cloudinaryService;
+        private readonly IMapper _mapper;
+
+        public PostController(IPostRepository postRepository, IPictureRepository pictureRepository, 
+            ICloudinaryService cloudinaryService, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _pictureRepository = pictureRepository;
+            _cloudinaryService = cloudinaryService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int) HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int) HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int) HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> AddPost([FromForm] PostForAddDto postForAddDto)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty);
+
+            var post = _mapper.Map<Post>(postForAddDto);
+
+            var formFiles = postForAddDto.Files.ToList();
+            
+            if (formFiles.Count > 10 || !formFiles.Any()) return BadRequest("Wrong number of files.");
+
+            if (formFiles.Any(file => file == null || file.Length <= 0))
+                return BadRequest("There was an error with the file(s).");
+
+            post.UserId = userId;
+
+            var result = await _postRepository.AddPost(post);
+
+            var pictures = new List<Picture>();
+
+            foreach (var file in formFiles)
+            {
+                await using var stream = file.OpenReadStream();
+                var uploadParameters = new ImageUploadParams
+                {
+                    File = new FileDescription(Guid.NewGuid().ToString(), stream),
+                    Transformation = new Transformation().Quality(50)
+                };
+                var uploadResult = await _cloudinaryService.UploadImageAsync(uploadParameters);
+
+                if (uploadResult == null || string.IsNullOrEmpty(uploadResult.PublicId))
+                {
+                    await _postRepository.DeletePost(result.Id);
+                    
+                    return BadRequest("There was an error while uploading Your photo.");
+                }
+                
+                pictures.Add(new Picture {PostId = result.Id, Url = uploadResult.Url.ToString()});
+            }
+            
+            var picturesResult = await _pictureRepository.AddPicturesForPost(pictures);
+
+            return (result != null) && picturesResult ? Ok() 
+                : BadRequest("There was an error while processing Your request.");
+        }
+    }
+}
---
+++ MyPics.Domain/DTOs/PostForAddDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace MyPics.Domain.DTOs
+{
+    public class PostForAddDto
+    {
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(1, 10)]
+        public int NumberOfPictures { get; set; }
+        [Required]
+        public IEnumerable<IFormFile> Files { get; set; }
+    }
+}
