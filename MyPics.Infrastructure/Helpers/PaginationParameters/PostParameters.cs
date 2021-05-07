fd62befb61af00ac34b49db3c1c6421509e6c384
Adam <adamo.b1997@gmail.com>
2021-05-07 15:26:45 +0200

    Create PostParameters.cs

---
+++ MyPics.Infrastructure/Helpers/PaginationParameters/PostParameters.cs
@@ -0,0 +1,21 @@
+namespace MyPics.Infrastructure.Helpers.PaginationParameters
+{
+    public class PostParameters
+    {
+        private const int MaxPageSize = 30;
+        private readonly int _pageNumber = 1;
+        private readonly int _pageSize = 21;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            init => _pageSize = value <= 0 ? 20 : value > MaxPageSize ? MaxPageSize : value;
+        }
+
+        public int PageNumber
+        {
+            get => _pageNumber;
+            init => _pageNumber = value <= 0 ? 1 : value;
+        }
+    }
+}
