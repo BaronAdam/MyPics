6d4a8c8c47e5373e4a313327fa9314f4d710a19a
Adam <adamo.b1997@gmail.com>
2021-05-11 11:36:58 +0200

    Add Migraiton

---
+++ MyPics.Infrastructure/Migrations/20210511093652_Comments.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPics.Infrastructure.Migrations
+{
+    public partial class Comments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatePosted",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DatePosted",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Comments");
+        }
+    }
+}
