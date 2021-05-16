bfbeaa062c458b54dc5e51040675c9c5165bda46
Adam <adamo.b1997@gmail.com>
2021-05-16 11:55:31 +0200

    Update DTOs

--- MyPics.Domain/DTOs/CommentForSingleDto.cs
+++ MyPics.Domain/DTOs/CommentForSingleDto.cs
@@ -9,6 +9,7 @@ namespace MyPics.Domain.DTOs
         public bool IsReply { get; set; }
         public int ParentCommentId { get; set; }
         public bool IsDeleted { get; set; }
+        public int NumberOfLikes { get; set; }
         public DateTime DatePosted { get; set; }
         public UserForCommentDto User { get; set; }
     }
